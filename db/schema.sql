abd6edea830f5750314fdc8721e672aee7e17dc8
Noe Leiva <leiva.noe.86@gmail.com>
2020-07-16 19:23:15 -0500

    created datbase files

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+DROP TABLE IF EXISTS burgers;
+
+CREATE TABLE burgers
+(
+    id INT AUTO_INCREMENT NOT NULL,
+        PRIMARY KEY(id),
+    burger_name VARCHAR(255) NOT NULL,
+    devoured BOOLEAN NOT NULL
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES("impossible awesome", false)
+,("animal style", false)
+,("charbroil", false)
